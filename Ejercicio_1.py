8e1620c282ae6775e4c52be3b94a360590314316
ItsmeRORO <107592530+MynameIsRORO@users.noreply.github.com>
2022-07-09 23:06:43 -0300

    Add files via upload

---
+++ Ejercicio_1.py
@@ -0,0 +1,13 @@
+"""
+    OpenBootcamp - Ruta Backend - Curso de Python
+
+                    Ejercicio 1
+                    
+"""
+
+nombre = 'Lea'
+
+print(f'Hola, soy {nombre}')
+print('Estoy empezando el curso de python')
+print('Espero aprender mucho')
+
